2336b447fd5f9b9bc479cf1bf9e43a494c6bb4a4
kganya maleka <kganyamilton@gmail.com>
2025-04-25 15:14:43 +0200

    directory structure updated according to projects requiremnets

---
+++ src/main/java/Validate_sa_id/ValidateSaId.java
@@ -0,0 +1,7 @@
+package Validate_sa_id;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class ValidateSaId {
+
+}
---
+++ src/test/java/ValidateSaIdTest.java
@@ -0,0 +1,2 @@
+public class ValidateSaIdTest {
+}
