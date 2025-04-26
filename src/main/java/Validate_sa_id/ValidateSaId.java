f8337ae7ad4be0fde1c888954ac73333f36a9ad1
kganya maleka <kganyamilton@gmail.com>
2025-04-26 12:50:52 +0200

    cleaning up code

--- src/main/java/Validate_sa_id/ValidateSaId.java
+++ src/main/java/Validate_sa_id/ValidateSaId.java
@@ -1,7 +1,5 @@
 package Validate_sa_id;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class ValidateSaId {
 
     public static boolean isIdNumberValid(String idNumber) {
