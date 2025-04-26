7bbd64f3ad58702d3f5ddfe8448816974b1053f7
kganya maleka <kganyamilton@gmail.com>
2025-04-26 14:03:59 +0200

    Method helper that only validate the date and uses realworld calender

--- src/main/java/Validate_sa_id/ValidateSaId.java
+++ src/main/java/Validate_sa_id/ValidateSaId.java
@@ -1,9 +1,29 @@
 package Validate_sa_id;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
+
 public class ValidateSaId {
 
     public static boolean isIdNumberValid(String idNumber) {
         return false;
+
     }
+    public static boolean isDateValid(String date) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyMMdd")
+                .withResolverStyle(ResolverStyle.STRICT);
+
+        try{
+            LocalDate localDate = LocalDate.parse(date,formatter);
+            return true;
+
+        }catch( DateTimeParseException e){
+            return false;
+        }
+    }
+
 
 }
